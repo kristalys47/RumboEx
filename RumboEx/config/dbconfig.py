ee89f9ff990b1cdb02d56652fc8aad47f379efaf
casanovaalejandra <alejandra.casanova@upr.edu>
2019-02-19 17:47:16 -0400

    db config new server

--- RumboEx/config/dbconfig.py
+++ RumboEx/config/dbconfig.py
@@ -1,10 +1,19 @@
 pg_config = {
-    'hostname': 'ec2-23-21-216-174.compute-1.amazonaws.com',
-    'user': 'ivbustqhsmsaps',
-    'password': '7a8951928430c500e432dbf97728f42f5033648c052a5befce59295cabd987c5',
-    'dbname': 'd9t2kdqh5u8ekk',
+    'hostname': 'rumboexadmin',
+    'user': 'rumboexadmin',
+    'password': 'pee0Laeg',
+    'dbname': 'mystudycoach',
     'port': '5432',
-    'host': 'ec2-23-21-216-174.compute-1.amazonaws.com',
-    'url': 'postgres://ivbustqhsmsaps:7a8951928430c500e432dbf97728f42f5033648c052a5befce59295cabd987c5@ec2-23-21-216-174.compute-1.amazonaws.com:5432/d9t2kdqh5u8ekk'
+    'host': '136.145.58.163'
 }
 
+
+#pg_config = {
+#    'hostname': 'ec2-23-21-216-174.compute-1.amazonaws.com',
+#   'user': 'ivbustqhsmsaps',
+#    'password': '7a8951928430c500e432dbf97728f42f5033648c052a5befce59295cabd987c5',
+#    'dbname': 'd9t2kdqh5u8ekk',
+#    'port': '5432',
+#    'host': 'ec2-23-21-216-174.compute-1.amazonaws.com',
+#    'url': 'postgres://ivbustqhsmsaps:7a8951928430c500e432dbf97728f42f5033648c052a5befce59295cabd987c5@ec2-23-21-216-174.compute-1.amazonaws.com:5432/d9t2kdqh5u8ekk'
+#}
